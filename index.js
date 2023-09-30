2d9fc9a38b59d070a85a5d3f1897058822a2a624
karol <01158930@pw.edu.pl>
2023-09-30 17:39:13 +0200

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWO8ei3KvnvajKNK1nYZsRUhaF4IOC0qM",
+  authDomain: "hackyeah-smartcity.firebaseapp.com",
+  projectId: "hackyeah-smartcity",
+  storageBucket: "hackyeah-smartcity.appspot.com",
+  messagingSenderId: "1012803374131",
+  appId: "1:1012803374131:web:b6c05958e99a0039a0e746"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
