44fc9d4c9b9a2a2d95ea34c9ff75aebe39d0594d
karol <01158930@pw.edu.pl>
2023-10-02 10:32:12 +0200

    clean

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDWO8ei3KvnvajKNK1nYZsRUhaF4IOC0qM",
-  authDomain: "hackyeah-smartcity.firebaseapp.com",
-  projectId: "hackyeah-smartcity",
-  storageBucket: "hackyeah-smartcity.appspot.com",
-  messagingSenderId: "1012803374131",
-  appId: "1:1012803374131:web:b6c05958e99a0039a0e746"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
